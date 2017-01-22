e31e8a8e70cfc1d2930e22f7df4518fea364e16b
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-01-22 21:58:48 +0100

    cleaning of man pages

--- R/ggforest.R
+++ R/ggforest.R
@@ -1,9 +1,11 @@
 #' Drawing Forest Plot for CoxPH model
 #' @description Drawing Forest Plot for CoxPH model
-#' @param fit an object of class coxph.
+#' @param model an object of class coxph.
 #' @param alpha significance level for coloring.
 #' @param ggtheme function, ggplot2 theme name. Default value is theme_classic2. Allowed values include ggplot2 official themes: see theme.
-#' @param legend.title legend title.
+#' @param plot.title legend title.
+#' @param palette the color palette to be used for coloring of significant variables.
+#' @param xlab Label in OX axis.
 #' @param font.main,font.submain,font.caption,font.x,font.y,font.tickslab a vector of length 3
 #'  indicating respectively the size (e.g.: 14), the style (e.g.: "plain",
 #'  "bold", "italic", "bold.italic") and the color (e.g.: "red") of main title, subtitle, caption,
@@ -26,6 +28,7 @@ ggforest <- function(model, alpha = 0.05,
                      plot.title = "Forest plot for coxph model",
                      ggtheme = theme_classic2(),
                      palette = c("black", "red4"),
+                     xlab = "Hazard ratio",
                      font.main = c(16, "plain", "black"), font.submain = c(15, "plain", "black"),
                      font.caption = c(15, "plain", "black"),
                      font.x = c(14, "plain", "black"), font.y = c(14, "plain", "black"),
@@ -35,15 +38,7 @@ ggforest <- function(model, alpha = 0.05,
   coef$est <- sapply(exp(coef$estimate), function(x) as.character(signif(x, digits = 2)))
   stars <- paste0(ifelse(coef$p.value < 0.05, "*",""),
                   ifelse(coef$p.value < 0.01, "*",""),
-                  ifelse(coef$p.value < 0.001, "*",""),
-                  xlab = "Hazard ratio",
-                  font.main = c(16, "bold", "darkblue"),          # font for titles of the plot, the table and censor part
-                  font.submain = c(15, "bold.italic", "purple"),  # font for subtitles in the plot, the table and censor part
-                  font.caption = c(14, "plain", "orange"),        # font for captions in the plot, the table and censor part
-                  font.x = c(14, "bold.italic", "red"),           # font for x axises in the plot, the table and censor part
-                  font.y = c(14, "bold.italic", "darkred"),       # font for y axises in the plot, the table and censor part
-                  font.tickslab = c(12, "plain", "darkgreen")    # font for ticklabs in the plot, the table and censor part
-  )
+                  ifelse(coef$p.value < 0.001, "*",""))
   coef$p.val <- paste0(coef$est, " (p.value ", coef$p.val, stars,")")
   coef$issig <- coef$p.value < alpha
 
@@ -62,6 +57,6 @@ ggforest <- function(model, alpha = 0.05,
     theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), legend.position = "none") +
     xlab("") +
     ggtitle(plot.title)
-  p <-.labs(p = p, font.main = font.main, font.x = font.x, font.y = font.y, font.submain = font.submain, font.caption = font.caption)
+  p <- .labs(p = p, font.main = font.main, font.x = font.x, font.y = font.y, font.submain = font.submain, font.caption = font.caption)
   p
 }
