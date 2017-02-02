4776ea18f69fdad0a9a5b795068b47caf482c814
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-02-02 10:08:07 +0100

    revert

--- R/ggcoxadjustedcurves.R
+++ R/ggcoxadjustedcurves.R
@@ -8,7 +8,6 @@
 #' For every observation in the dataset a prediction for survival cure is made.
 #' Then the predictions are averaged with respect to a selected variable.
 #'@param fit an object of class \link{coxph.object} - created with \link{coxph} function.
-#'@param data a dataset for predictions. If not supplied then data will be extracted from `fit` object.
 #'@param split_var a variable (vector) with values corresponding to groups to be plotted
 #'@param plot_all if TRUE then all individual predicted survival curves will be plotted
 #'@param curve.size,curve.alpha size and alpha for individual survival curves
@@ -31,10 +30,10 @@
 #' fit <- coxph( Surv(time, status) ~ rx + adhere, data = colon )
 #'
 #' ggcoxadjustedcurves(fit)
+#' ggcoxadjustedcurves(fit, plot_all=TRUE)
 #'
 #' fit2 <- coxph( Surv(stop, event) ~ rx + size, data = bladder )
 #' ggcoxadjustedcurves(fit2)
-#' ggcoxadjustedcurves(fit2, plot_all=TRUE, data = bladder, curve.alpha=0.01)
 #' ggcoxadjustedcurves(fit2, plot_all=TRUE, curve.alpha=0.01)
 #' ggcoxadjustedcurves(fit2, split_var = factor( bladder[,"rx"]))
 #' ggcoxadjustedcurves(fit2, split_var = factor(bladder[,"rx"]), plot_all=TRUE, curve.alpha=0.01)
@@ -43,14 +42,16 @@
 ggcoxadjustedcurves <- function(fit,
                              split_var = NULL,
                              plot_all = FALSE,
-                             data = NULL,
                              curve.size = 2, curve.alpha = 0.2,
                              font.main = c(16, "plain", "black"),
                              font.x = c(14, "plain", "black"), font.y = c(14, "plain", "black"),
                              font.tickslab = c(12, "plain", "black"),
                              ylab = "Survival rate",
                              ggtheme = theme_classic2()){
-  data <- .get_data(fit, data)
+  # this is a very risky way to get the data for a model
+  # it works only in the call$data exists in the search path and may be evaluated
+  # unfortunately it is quite common in this package and I do not see a better way ;-)
+  data <- eval(fit$call$data)
 
   pred <- survfit(fit, data)
   timepoints <- c(0, pred$time)
@@ -58,16 +59,14 @@ ggcoxadjustedcurves <- function(fit,
   adj_surv <- as.data.frame(cbind(time0 = 1, t(pred$surv)))
   colnames(adj_surv) <- paste0("time", timepoints)
 
-  # this dirty hack hides the problem with CHECK NOTES generated for
-  # non standard evaluation used by dplyr/tidyr
-  .id <- time <- value <- NULL
+  both <- cbind(data, adj_surv)
 
   if (is.null(split_var)) {
     # only one curve
     both <- cbind(.id = seq(data[,1]), adj_surv)
     curves <- gather(both, time, value, -.id)
     curves$time <- as.numeric(gsub(curves$time, pattern = "time", replacement = ""))
-    curve <- summarise(group_by(curves, time), value = mean(value, na.rm=TRUE))
+    curve <- summarise(group_by(curves, time), value = mean(value))
     pl <- ggplot(curve, aes(x = time, y = value)) +
       geom_step(size=curve.size)
   } else {
@@ -75,7 +74,7 @@ ggcoxadjustedcurves <- function(fit,
     both <- cbind(.id = seq(data[,1]), split_var, adj_surv)
     curves <- gather(both, time, value, -.id, -split_var)
     curves$time <- as.numeric(gsub(curves$time, pattern = "time", replacement = ""))
-    curve <- summarise(group_by(curves, time, split_var), value = mean(value, na.rm=TRUE))
+    curve <- summarise(group_by(curves, time, split_var), value = mean(value))
     pl <- ggplot(curve, aes(x = time, y = value, color=split_var)) +
       geom_step(size=curve.size)
   }
