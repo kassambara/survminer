ac27f0cb3fdb80fad180d16197d77330e5cbdcec
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-02-27 20:24:32 +0100

    ggforest: graphical parameters are now passed to ggpar #151

--- R/ggforest.R
+++ R/ggforest.R
@@ -6,12 +6,7 @@
 #' @param plot.title legend title.
 #' @param palette the color palette to be used for coloring of significant variables.
 #' @param xlab Label in OX axis.
-#' @param font.main,font.submain,font.caption,font.x,font.y,font.tickslab a vector of length 3
-#'  indicating respectively the size (e.g.: 14), the style (e.g.: "plain",
-#'  "bold", "italic", "bold.italic") and the color (e.g.: "red") of main title, subtitle, caption,
-#'  xlab and ylab and axis tick labels, respectively. For example \emph{font.x =
-#'  c(14, "bold", "red")}.  Use font.x = 14, to change only font size; or use
-#'  font.x = "bold", to change only font face.
+#'@param ... further arguments passed to the function \code{\link[ggpubr]{ggpar}} for customizing the plot.
 #'
 #' @return return an object of class ggplot
 #'
@@ -28,13 +23,9 @@
 
 ggforest <- function(model, alpha = 0.05,
                      plot.title = "Forest plot for coxph model",
-                     ggtheme = theme_classic2(),
+                     ggtheme = theme_survminer(),
                      palette = c("black", "red4"),
-                     xlab = "Hazard ratio",
-                     font.main = c(16, "plain", "black"), font.submain = c(15, "plain", "black"),
-                     font.caption = c(15, "plain", "black"),
-                     font.x = c(14, "plain", "black"), font.y = c(14, "plain", "black"),
-                     font.tickslab = c(12, "plain", "black")) {
+                     xlab = "Hazard ratio", ...) {
   coef <- tidy(model)
   coef$p.val <- sapply(coef$p.value, function(x) as.character(signif(x, digits = 2)))
   coef$est <- sapply(exp(coef$estimate), function(x) as.character(signif(x, digits = 2)))
@@ -60,7 +51,6 @@ ggforest <- function(model, alpha = 0.05,
     theme(panel.grid.minor = element_blank(), panel.grid.major.y = element_blank(), legend.position = "none") +
     xlab("") +
     ggtitle(plot.title)
-  p <- .labs(p = p, font.main = font.main, font.x = font.x, font.y = font.y, font.submain = font.submain, font.caption = font.caption)
-  p <- .set_ticks(p, font.tickslab = font.tickslab)
+  p <- ggpubr::ggpar(p, ...)
   p
 }
