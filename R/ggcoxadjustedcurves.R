56c1d8bd8d6c2b2e0188b0e610951704309d123c
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-03-18 11:36:37 +0100

    fix for #174

--- R/ggcoxadjustedcurves.R
+++ R/ggcoxadjustedcurves.R
@@ -79,6 +79,6 @@ ggcoxadjustedcurves <- function(fit,
   pl <- pl + ggtheme +
     scale_y_continuous(limits = c(0, 1)) +
     ylab(ylab)
-  ggpubr::ggpar(pl,  ...)
+  ggpubr::ggpar(pl,  palette = palette, ...)
 
 }
