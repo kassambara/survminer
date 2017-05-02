881d25c4a7a9afee9f1eddf12e7f22e1d4dc27f3
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-05-02 10:40:55 +0200

    fix for #208

--- R/ggsurvplot_core.R
+++ R/ggsurvplot_core.R
@@ -324,7 +324,7 @@ ggsurvplot_core <- function(fit, data = NULL, fun = NULL,
   ggsurv <- gridExtra::arrangeGrob(grobs = grobs, nrow = nplot, heights = unlist(heights))
 
   # Set legend
-  if(nplot > 1 & legend.position %in% c("left", "right", "bottom") & is.null(legend.grob)){
+  if(nplot > 1 & legend.position %in% c("left", "right", "bottom") & !is.null(legend.grob)){
     ggsurv <- switch(legend.position,
                      bottom = gridExtra::arrangeGrob(grobs = list(ggsurv, legend.grob), nrow = 2, heights = c(0.9, 0.1)),
                      top = gridExtra::arrangeGrob(grobs = list(legend.grob, ggsurv), nrow = 2, heights = c(0.1, 0.9)),
