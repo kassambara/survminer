426cc9c07e3d146b079ae4077e41c667e3467c6b
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-06-06 19:35:59 +0200

    fix for #225

--- R/ggsurvtheme.R
+++ R/ggsurvtheme.R
@@ -138,6 +138,7 @@ theme_cleantable <- function(base_size = 12, base_family = "", ...)
       axis.line.y = element_blank(),
       axis.text.x = element_blank(),
       axis.ticks.x = element_blank(),
+      axis.ticks.y = element_blank(),
       axis.title.x = element_blank(),
       axis.title.y = element_blank()
     )
