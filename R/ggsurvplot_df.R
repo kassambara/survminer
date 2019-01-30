849ef9a8e7d9f84f69adde83c5de818852246178
Bingxin S <45691876+BingxinS@users.noreply.github.com>
2019-01-30 11:53:35 -0500

    Update ggsurvplot_df.R
    Now .connect2origin() can also handle non-numerical strata and facet factors.

--- R/ggsurvplot_df.R
+++ R/ggsurvplot_df.R
@@ -319,7 +319,7 @@ ggsurvplot_df <- function(fit, fun = NULL,
     if(indStrata<indMax & n.strata>0){
       for (indRow in c(1:n.strata)) {
         tmpStrata = as.list(strsplit(strata[indRow], '\\,'))[[1]]
-        tmpFacet  = as.numeric(gsub("\\D", "", tmpStrata)) 
+        tmpFacet  = gsub(".*=", "", tmpStrata)
         base[indRow, c((indStrata+1):indMax)] = tmpFacet
       }
     }
