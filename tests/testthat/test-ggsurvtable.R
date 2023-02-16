7c4ad1571a1aede654028685bf6cfb28451cc940
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2023-02-16 07:59:01 +0100

    fix lung data not found in unit test

--- tests/testthat/test-ggsurvtable.R
+++ tests/testthat/test-ggsurvtable.R
@@ -1,11 +1,12 @@
 context("test-ggsurvtable")
 
-library(dplyr)
-library("survival")
-data("lung")
-fit <- survfit(Surv(time, status) ~ sex, data = lung)
+
 
 test_that("survtable y axis label colors work", {
+  library(dplyr)
+  library("survival")
+  #data("lung", package = "survival")
+  fit <- survfit(Surv(time, status) ~ sex, data = lung)
   p <- ggrisktable(fit, data = lung, color = "strata")
   .build <- ggplot_build(p)
   .build_data <- .build$data[[1]]
